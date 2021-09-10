0a9d9e5bfd3154436186cafcd7a383e7abb29de4
X0FloH <33568643+X0FloH@users.noreply.github.com>
2021-09-10 19:12:09 +0100

    Menu bar exit now uses prompt

--- sim.py
+++ sim.py
@@ -951,7 +951,7 @@ filemenu.add_separator()
 filemenu.add_command(label="Save", command=SaveToFile)
 filemenu.add_command(label="Save As..", command=SaveToFileNoCurrent)
 filemenu.add_separator()
-filemenu.add_command(label="Exit", command=window.destroy)
+filemenu.add_command(label="Exit", command=CloseSaveInst)
 menubar.add_cascade(label="File", menu=filemenu)
 
 editmenu = tk.Menu(menubar, tearoff=0)
